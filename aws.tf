4dec41ff967fb7c30402cd38094e2236aedf711f
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-30 10:37:19 +0530

    Add files via upload

--- aws.tf
+++ aws.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-    access_key = var.active_key
-    secret_key = var.security_key
-    region = var.aws_region
+	access_key = var.active_key
+	secret_key = var.security_key
+	region = var.aws_region              
 }
+
+
