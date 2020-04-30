f25d917df4e703d8f432fd64d05569f00768d75e
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-30 10:50:21 +0530

    Delete dbsubnetsecuritygroup.tf

--- dbsubnetsecuritygroup.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_db_subnet_group" "tfdbsubnetgroup" {
-name = "rdssg"
-subnet_ids = [aws_subnet.tfprivatesubnet.id, aws_subnet.tfpublicsubnet.id, aws_subnet.tfpublicsubnet2.id] 
-
-tags {
-       Name = "rdssubnetgrp"
-     }
-}
