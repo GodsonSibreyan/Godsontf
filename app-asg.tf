69598e948feea63f3da6f35d41849cdfb4a1c95f
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-05-08 14:19:08 +0530

    Update app-asg.tf

--- app-asg.tf
+++ app-asg.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "app" {
     from_port   = "22"
     to_port     = "22"
     protocol    = "tcp"
-    cidr_blocks = module.vpc.public_subnets
+    cidr_blocks = module.vpc.public_subnets_cidr_blocks
   }
 
   egress {
