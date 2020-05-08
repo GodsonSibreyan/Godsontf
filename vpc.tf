df1a4ed7381d5c41ecf09f8bc386a1e03f5d0ef1
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-05-08 12:45:45 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -26,22 +26,22 @@ variable "vpc_cidr" {
 
 variable "vpc_public_subnets" {
   description = "A list of public subnets inside the VPC"
-  default     = [10.0.1.0/24, 10.0.2.0/24]
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "vpc_private_subnets" {
   description = "A list of private subnets inside the VPC"
-  default     = [10.0.11.0/24, 10.0.12.0/24]
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "vpc_database_subnets" {
   description = "A list of database subnets"
-  default     = [10.0.21.0/24, 10.0.22.0/24"]
+  default     = ["10.0.21.0/24", "10.0.22.0/24"]
 }
 
 variable "vpc_azs" {
   description = "A list of availability zones in the region"
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c","us-east-1d","us-east-1e","us-east-1f"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "vpc_enable_nat_gateway" {
